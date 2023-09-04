988dc0cf0aaaad3c3bf3e18c6a2230f24a99d041
changsubkwak <paradise1732@gmail.com>
2023-09-04 23:43:26 +0900

    feat: add hash function

--- main/ListNode.py
+++ main/ListNode.py
@@ -1,19 +1,22 @@
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+  def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = next
 
-    def __eq__(self, other):
-        c1 = self
-        c2 = other
+  def __eq__(self, other):
+    c1 = self
+    c2 = other
 
-        while c1 is not None and c2 is not None:
-            if c1.val != c2.val:
-                return False
-            c1 = c1.next
-            c2 = c2.next
-
-        if c1 is None and c2 is None:
-            return True
-
+    while c1 is not None and c2 is not None:
+      if c1.val != c2.val:
         return False
+      c1 = c1.next
+      c2 = c2.next
+
+    if c1 is None and c2 is None:
+      return True
+
+    return False
+
+  def __hash__(self):
+    return id(self)
