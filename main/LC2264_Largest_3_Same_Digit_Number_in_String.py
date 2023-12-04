badec2bfb3dd5240e5ce5de7a61dcb9d388f15ef
paradise1732 <paradise1732@gmail.com>
2023-12-04 19:00:47 +0900

    feat: solved - 2264. Largest 3-Same-Digit Number in String

--- main/LC2264_Largest_3_Same_Digit_Number_in_String.py
+++ main/LC2264_Largest_3_Same_Digit_Number_in_String.py
@@ -1,3 +1,15 @@
-class LC2147_Number_of_Ways_to_Divide_a_Long_Corridor:
-    def numberOfWays(self, corridor: str) -> int:
-        return 0
+class LC2264_Largest_3_Same_Digit_Number_in_String:
+    def isSameThree(self, num: str) -> bool:
+        return num[0] == num[1] and num[1] == num[2]
+
+
+    def largestGoodInteger(self, num: str) -> str:
+        max = ""
+        for i in range(len(num) - 2):
+            part = num[i:i+3]
+            if self.isSameThree(part):
+                if max == "" or max <= part:
+                    max = part
+
+        return max
+
