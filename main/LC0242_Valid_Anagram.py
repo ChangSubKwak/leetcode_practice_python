713cc94a8da2aa36521fc8a12b52f124414f6076
changsubkwak <paradise1732@gmail.com>
2023-12-17 23:46:41 +0900

    feat: solved - 242. Valid Anagram

--- main/LC0242_Valid_Anagram.py
+++ main/LC0242_Valid_Anagram.py
@@ -1,3 +1,20 @@
 class LC0242_Valid_Anagram:
-    def isAnagram(self, s:str, t:str) -> bool:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sourceCount = [0 for _ in range(26)]
+        targetCount = [0 for _ in range(26)]
+
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            sourceIndex = ord(s[i]) - ord('a')
+            sourceCount[sourceIndex] += 1
+
+            targetIndex = ord(t[i]) - ord('a')
+            targetCount[targetIndex] += 1
+
+        for i in range(26):
+            if sourceCount[i] != targetCount[i]:
+                return False
+
         return True
