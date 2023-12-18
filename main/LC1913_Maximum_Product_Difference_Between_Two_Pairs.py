989df88e510543cfeda21d1887c8ac41a9d095b1
changsubkwak <paradise1732@gmail.com>
2023-12-19 03:28:34 +0900

    feat: solved - 1913. Maximum Product Difference Between Two Pairs

--- main/LC1913_Maximum_Product_Difference_Between_Two_Pairs.py
+++ main/LC1913_Maximum_Product_Difference_Between_Two_Pairs.py
@@ -3,4 +3,12 @@ from typing import List
 
 class LC1913_Maximum_Product_Difference_Between_Two_Pairs:
     def maxProductDifference(self, nums: List[int]) -> int:
-        return 34
+        length = len(nums)
+        nums.sort()
+
+        w = nums[0]
+        x = nums[1]
+        y = nums[length - 2]
+        z = nums[length - 1]
+
+        return y * z - w * x
