4f29c74b7075e1b26aa93736046a434a56dfa63a
changsubkwak <paradise1732@gmail.com>
2023-12-21 00:02:49 +0900

    test: implement test case - 2709. Buy Two Chocolates

---
+++ test/LC2709_Buy_Two_Chocolates_Test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from main.LC2709_Buy_Two_Chocolates import LC2709_Buy_Two_Chocolates
+
+test = LC2709_Buy_Two_Chocolates()
+
+
+class LC2709_Buy_Two_Chocolates_Test(unittest.TestCase):
+    def test_onesMinusZeros(self):
+        self.assertEqual(test.buyChoco([1, 2, 2], 3), 0)
+        self.assertEqual(test.buyChoco([3, 2, 3], 3), 3)
+        self.assertEqual(test.buyChoco([74, 31, 38, 24, 25, 24, 5], 79), 50)
