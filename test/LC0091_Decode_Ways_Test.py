0b6622a0f67495de55706c8585662bc749719939
changsubkwak <paradise1732@gmail.com>
2023-12-25 23:42:09 +0900

    test: implement test case - 91. Decode Ways

---
+++ test/LC0091_Decode_Ways_Test.py
@@ -0,0 +1,11 @@
+import unittest
+
+from main.LC0091_Decode_Ways import LC0091_Decode_Ways
+
+test = LC0091_Decode_Ways()
+
+
+class LC0091_Decode_Ways_Test(unittest.TestCase):
+  def test_numDecodings(self):
+    self.assertEqual(test.numDecodings("12"), 2)
+    self.assertEqual(test.numDecodings("226"), 3)
