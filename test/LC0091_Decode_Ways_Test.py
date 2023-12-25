c3f657cfe25c3977ae2918fd97a1377d112a30d6
changsubkwak <paradise1732@gmail.com>
2023-12-26 00:25:46 +0900

    test: add test case - 91. Decode Ways

--- test/LC0091_Decode_Ways_Test.py
+++ test/LC0091_Decode_Ways_Test.py
@@ -9,3 +9,4 @@ class LC0091_Decode_Ways_Test(unittest.TestCase):
   def test_numDecodings(self):
     self.assertEqual(test.numDecodings("12"), 2)
     self.assertEqual(test.numDecodings("226"), 3)
+    self.assertEqual(test.numDecodings("06"), 0)
