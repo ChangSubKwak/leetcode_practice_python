7507ac7219133969708caf460154b02bccdec2df
changsubkwak <paradise1732@gmail.com>
2024-01-03 01:24:34 +0900

    feat: solved - 455. Assign Cookies

--- main/LC0455_Assign_Cookies.py
+++ main/LC0455_Assign_Cookies.py
@@ -5,4 +5,18 @@ class LC0455_Assign_Cookies:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort()
         s.sort()
-        return 1
+
+        count = 0
+        gIndex = 0
+        sIndex = 0
+
+        while gIndex < len(g) and sIndex < len(s):
+            if g[gIndex] <= s[sIndex]:
+                gIndex += 1
+                sIndex += 1
+                count += 1
+                continue
+
+            sIndex += 1
+
+        return count
