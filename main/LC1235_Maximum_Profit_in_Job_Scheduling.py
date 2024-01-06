990572e9dab82b1c1eef61b64676a6df55f8d78e
changsubkwak <paradise1732@gmail.com>
2024-01-06 23:37:38 +0900

    feat: (WIP) solving - 1235. Maximum Profit in Job Scheduling

---
+++ main/LC1235_Maximum_Profit_in_Job_Scheduling.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class LC1235_Maximum_Profit_in_Job_Scheduling:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        return 120
