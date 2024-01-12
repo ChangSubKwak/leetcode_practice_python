06134715b15af331379e6d7971d72008f0eb428c
changsubkwak <paradise1732@gmail.com>
2024-01-12 23:54:23 +0900

    feat: solved - 446. Arithmetic Slices II Subsequence

--- main/LC0446_Arithmetic_Slices_II_Subsequence.py
+++ main/LC0446_Arithmetic_Slices_II_Subsequence.py
@@ -3,4 +3,19 @@ from typing import List
 
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        return 16
+        length = len(nums)
+        count = 0
+        dp = [{} for _ in range(length)]
+
+        for i in range(1, length):
+            for j in range(i):
+                difference = nums[i] - nums[j]
+                if difference not in dp[i]:
+                    dp[i][difference] = 0
+                dp[i][difference] += 1
+
+                if difference in dp[j]:
+                    dp[i][difference] += dp[j][difference]
+                    count += dp[j][difference]
+
+        return count
