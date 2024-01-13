7156938538dd925b9164c1cfe1c05280f440d8d2
changsubkwak <paradise1732@gmail.com>
2024-01-13 23:49:01 +0900

    test: implement test case - 1347. Minimum Number of Steps to Make Two Strings Anagram

---
+++ test/LC1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram_Test.py
@@ -0,0 +1,12 @@
+import unittest
+from main.LC1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram import Solution
+from main.ListNode import ListNode
+
+test = Solution()
+
+
+class Test_LC1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram(unittest.TestCase):
+    def test_minSteps(self):
+        self.assertEqual(test.minSteps("bab", "aba"), 1)
+        self.assertEqual(test.minSteps("leetcode", "practice"), 5)
+        self.assertEqual(test.minSteps("anagram", "mangaar"), 0)
