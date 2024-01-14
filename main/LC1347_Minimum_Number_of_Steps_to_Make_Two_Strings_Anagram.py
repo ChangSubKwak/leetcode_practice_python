362176d20af6ff6f7c39f6d636655a28e7aa3000
changsubkwak <paradise1732@gmail.com>
2024-01-14 20:24:37 +0900

    feat: solved - 1347. Minimum Number of Steps to Make Two Strings Anagram

--- main/LC1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram.py
+++ main/LC1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram.py
@@ -1,3 +1,20 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        return 1
+        length = len(t)
+        countSource = [0] * 26
+        countTarget = [0] * 26
+
+        for i in range(length):
+            countSource[ord(s[i]) - 97] += 1
+            countTarget[ord(t[i]) - 97] += 1
+
+        # print(countSource)
+        # print(countTarget)
+
+        result = 0
+        for i in range(26):
+            result += abs(countSource[i] - countTarget[i])
+
+        result /= 2
+
+        return int(result)
