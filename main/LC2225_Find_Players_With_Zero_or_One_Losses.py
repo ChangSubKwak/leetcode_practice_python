45ad11ebc4068ac2a012e79f27805c50f4bd92a7
cskwak <changsubkwak@nongshim.co.kr>
2024-01-16 16:06:30 +0900

    feat: solved - 2225. Find Players With Zero or One Losses

--- main/LC2225_Find_Players_With_Zero_or_One_Losses.py
+++ main/LC2225_Find_Players_With_Zero_or_One_Losses.py
@@ -3,4 +3,38 @@ from typing import List
 
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        return [[]]
+        result = [[], []]
+        winnerSet = set()
+        loserSet = set()
+
+        lostStat = {}
+        for match in matches:
+            winner = match[0]
+            if winner not in winnerSet:
+                winnerSet.add(winner)
+
+            loser = match[1]
+            if loser not in loserSet:
+                loserSet.add(loser)
+
+            if match[1] not in lostStat:
+                lostStat[match[1]] = 0
+            lostStat[match[1]] += 1
+
+        for match in matches:
+            loser = match[1]
+
+            if loser in winnerSet:
+                winnerSet.remove(loser)
+
+        for winner in winnerSet:
+            result[0].append(winner)
+
+        for key, value in lostStat.items():
+            if value == 1:
+                result[1].append(key)
+
+        result[0].sort()
+        result[1].sort()
+
+        return result
