24a4154165358be73dc96caf5472119dbf53cabb
changsubkwak <paradise1732@gmail.com>
2024-01-23 23:44:05 +0900

    test: implement test case - 1239. Maximum Length of a Concatenated String with Unique Characters

---
+++ test/LC1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters_Test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from main.LC1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters import Solution
+
+test = Solution()
+
+
+class SolutionTest(unittest.TestCase):
+  def test(self):
+    self.assertEqual(test.maxLength(["un", "iq", "ue"]), 4)
+    self.assertEqual(test.maxLength(["cha", "r", "act", "ers"]), 6)
+    self.assertEqual(test.maxLength(["abcdefghijklmnopqrstuvwxyz"]), 26)
+
+
+
