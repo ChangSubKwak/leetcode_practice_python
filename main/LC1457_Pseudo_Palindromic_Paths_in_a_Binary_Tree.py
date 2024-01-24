af78f1846397e21d3b6244b28723900b7d1671ce
changsubkwak <paradise1732@gmail.com>
2024-01-25 00:27:14 +0900

    feat: solved - 1457 Pseudo Palindromic Paths in a Binary Tree

--- main/LC1457_Pseudo_Palindromic_Paths_in_a_Binary_Tree.py
+++ main/LC1457_Pseudo_Palindromic_Paths_in_a_Binary_Tree.py
@@ -4,5 +4,17 @@ from main.TreeNode import TreeNode
 
 
 class Solution:
+    def preorder(self, node, path):
+        if node:
+            path = path ^ (1 << node.val)
+            if node.left is None and node.right is None:
+                if path & (path - 1) == 0:
+                    self.count += 1
+            else:
+                self.preorder(node.left, path)
+                self.preorder(node.right, path)
+
     def pseudoPalindromicPaths(self, root: Optional[TreeNode]) -> int:
-        return 2
+        self.count = 0
+        self.preorder(root, 0)
+        return self.count
