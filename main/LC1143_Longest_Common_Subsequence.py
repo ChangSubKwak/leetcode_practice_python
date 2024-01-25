b1ee5a8463d9c69f8ca880599b6343f62fc2d77f
changsubkwak <paradise1732@gmail.com>
2024-01-25 23:36:33 +0900

    feat: solved - 1143. Longest Common Subsequence

--- main/LC1143_Longest_Common_Subsequence.py
+++ main/LC1143_Longest_Common_Subsequence.py
@@ -1,3 +1,14 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        return 0
+        m, n = map(len, (text1, text2))
+
+        if m < n:
+            text1, text2 = text2, text1
+
+        dp = [0] * (len(text2) + 1)
+        for c1 in text1:
+            prev = 0
+            for i, c2 in enumerate(text2):
+                prev, dp[i + 1] = dp[i + 1], prev + 1 if c1 == c2 else max(dp[i], dp[i + 1])
+
+        return dp[-1]
