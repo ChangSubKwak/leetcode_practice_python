fd910e97fd2cf21185e0fe00f83968c978615905
cskwak <changsubkwak@nongshim.co.kr>
2024-02-01 22:49:13 +0900

    test: implement test case - 2966. Divide Array Into Arryas With Max Difference

---
+++ test/LC2966_Divide_Array_Into_Arrays_With_Max_Difference_Test.py
@@ -0,0 +1,11 @@
+import unittest
+
+from main.LC2966_Divide_Array_Into_Arrays_With_Max_Difference import Solution
+
+test = Solution()
+
+
+class SolutionTest(unittest.TestCase):
+    def test_minOperations(self):
+        self.assertIn(test.divideArray([1, 3, 4, 8, 7, 9, 3, 5, 1], 2), [[1, 1, 3], [3, 4, 5], [7, 8, 9]])
+        self.assertIn(test.divideArray([1, 3, 3, 2, 7, 3], 3), [])
