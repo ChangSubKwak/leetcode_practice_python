11d34933a449ab0fb857e79c84b1e65f64dbb9f9
changsubkwak <paradise1732@gmail.com>
2024-02-06 00:55:35 +0900

    feat: solved - 387. First Unique Character in a String

--- main/LC0387_First_Unique_Character_in_a_String.py
+++ main/LC0387_First_Unique_Character_in_a_String.py
@@ -2,5 +2,18 @@ from typing import List
 
 
 class Solution:
-    def firstUniqChar(self, nums: List[int]) -> int:
-        return 1
+    def firstUniqChar(self, s: str) -> int:
+        charIndex = [-1] * 26
+        countChar = [0] * 26
+
+        for i, c in enumerate(s):
+            index = ord(c) - ord('a')
+            charIndex[index] = i
+            countChar[index] += 1
+
+        for c in s:
+            index = ord(c) - ord('a')
+            if countChar[index] == 1:
+                return charIndex[index]
+
+        return -1
