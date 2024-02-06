29d9565c7396775d08528c348c833c73ca14bd08
changsubkwak <paradise1732@gmail.com>
2024-02-07 00:19:37 +0900

    feat: solved - 49. Group Anagrams

--- main/LC0049_Group_Anagrams.py
+++ main/LC0049_Group_Anagrams.py
@@ -3,4 +3,18 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        return [[]]
+        stringMap = {}
+
+        for string in strs:
+            sortedString = "".join(sorted(string))
+
+            if sortedString not in stringMap:
+                stringMap[sortedString] = []
+
+            stringMap[sortedString].append(string)
+
+        result = []
+        for key, value in stringMap.items():
+            result.append(sorted(value))
+
+        return result
