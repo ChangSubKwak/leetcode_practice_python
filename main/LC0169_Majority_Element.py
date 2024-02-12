ec298973ce6ed6767666881d8e8ed543b19f4896
changsubkwak <paradise1732@gmail.com>
2024-02-13 00:52:23 +0900

    feat: solved - 169. Majority Element

--- main/LC0169_Majority_Element.py
+++ main/LC0169_Majority_Element.py
@@ -3,4 +3,17 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return 0
+        count = {}
+        length = len(nums)
+        for num in nums:
+            if num not in count:
+                count[num] = 0
+            count[num] += 1
+
+        result = nums[0]
+        for key, value in count.items():
+            if value > (length / 2):
+                result = key
+                break
+
+        return result
