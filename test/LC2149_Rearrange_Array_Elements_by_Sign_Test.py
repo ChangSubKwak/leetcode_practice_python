1ccbc4a649fcea140986ef1c16386c24689d8e2a
cskwak <changsubkwak@nongshim.co.kr>
2024-02-14 21:42:22 +0900

    test: implement test case - 2149. Rearrange Array Elements by Sign

---
+++ test/LC2149_Rearrange_Array_Elements_by_Sign_Test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from main.LC2149_Rearrange_Array_Elements_by_Sign import Solution
+
+test = Solution()
+
+
+class SolutionTest(unittest.TestCase):
+    def test_numberOfBeams(self):
+        self.assertEqual(test.rearrangeArray([3, 1, -2, -5, 2, -4]), [3, -2, 1, -5, 2, -4])
+        self.assertEqual(test.rearrangeArray([-1, 1]), [1, -2])
+
