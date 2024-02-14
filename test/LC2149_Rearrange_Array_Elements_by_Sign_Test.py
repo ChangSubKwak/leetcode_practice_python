63e83db74eee8e76d0cb68dd20163ba19e73c9ce
changsubkwak <paradise1732@gmail.com>
2024-02-15 01:27:59 +0900

    test: fix test code - 2149. Rearrange Array Elements by Sign

--- test/LC2149_Rearrange_Array_Elements_by_Sign_Test.py
+++ test/LC2149_Rearrange_Array_Elements_by_Sign_Test.py
@@ -8,5 +8,5 @@ test = Solution()
 class SolutionTest(unittest.TestCase):
     def test_numberOfBeams(self):
         self.assertEqual(test.rearrangeArray([3, 1, -2, -5, 2, -4]), [3, -2, 1, -5, 2, -4])
-        self.assertEqual(test.rearrangeArray([-1, 1]), [1, -2])
+        self.assertEqual(test.rearrangeArray([-1, 1]), [1, -1])
 
