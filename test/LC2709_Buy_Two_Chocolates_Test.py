38420f42e1272546d2ae10c15f5da1b150c1dd46
changsubkwak <paradise1732@gmail.com>
2024-02-25 23:53:35 +0900

    fix: change problem number

--- test/LC2709_Buy_Two_Chocolates_Test.py
+++ test/LC2709_Buy_Two_Chocolates_Test.py
@@ -1,8 +1,8 @@
 import unittest
 
-from main.LC2709_Buy_Two_Chocolates import LC2709_Buy_Two_Chocolates
+from main.LC2706_Buy_Two_Chocolates import LC2706_Buy_Two_Chocolates
 
-test = LC2709_Buy_Two_Chocolates()
+test = LC2706_Buy_Two_Chocolates()
 
 
 class LC2709_Buy_Two_Chocolates_Test(unittest.TestCase):
