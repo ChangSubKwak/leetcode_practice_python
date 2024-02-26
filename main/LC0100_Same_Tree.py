089a1990b2719b00384555fd0a5185e0c8054b63
changsubkwak <paradise1732@gmail.com>
2024-02-27 00:20:05 +0900

    feat: solved - 100. Same Tree

--- main/LC0100_Same_Tree.py
+++ main/LC0100_Same_Tree.py
@@ -4,5 +4,15 @@ from main.TreeNode import TreeNode
 
 
 class Solution:
+    def recur(self, p, q):
+        if (not p and q) or (p and not q):
+            return False
+
+        if not p and not q:
+            return True
+
+        comparison = p.val == q.val
+        return comparison and self.recur(p.left, q.left) and self.recur(p.right, q.right)
+
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        return True
+        return self.recur(p, q)
