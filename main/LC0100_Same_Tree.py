8c923841a3a079de7348a079a3a07d09c8151b4f
changsubkwak <paradise1732@gmail.com>
2024-02-26 23:05:57 +0900

    feat: (WIP) solving - 100. Same Tree

---
+++ main/LC0100_Same_Tree.py
@@ -0,0 +1,8 @@
+from typing import Optional
+
+from main.TreeNode import TreeNode
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        return True
