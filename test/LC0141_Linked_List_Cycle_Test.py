7116b12021facb4c62d464fade1ff9524dbcb036
cskwak <changsubkwak@nongshim.co.kr>
2024-03-06 21:40:37 +0900

    test: change class name - 141. Linked List Cycle

--- test/LC0141_Linked_List_Cycle_Test.py
+++ test/LC0141_Linked_List_Cycle_Test.py
@@ -1,27 +1,27 @@
 import unittest
 
-from main.LC0141_Linked_List_Cycle import LC0141_Linked_List_Cycle
+from main.LC0141_Linked_List_Cycle import Solution
 from main.ListNode import ListNode
 
-test = LC0141_Linked_List_Cycle()
+test = Solution()
 
 
-class test_LC0141_Linked_List_Cycle(unittest.TestCase):
-  def test_has_cycle(self):
-    listnode = ListNode(3)
-    listnode.next = ListNode(2)
-    listnode.next.next = ListNode(0)
-    listnode.next.next.next = ListNode(-4)
-    listnode.next.next.next.next = listnode.next
+class SolutionTest(unittest.TestCase):
+    def test_has_cycle(self):
+        listnode = ListNode(3)
+        listnode.next = ListNode(2)
+        listnode.next.next = ListNode(0)
+        listnode.next.next.next = ListNode(-4)
+        listnode.next.next.next.next = listnode.next
 
-    self.assertEqual(test.hasCycle(listnode), True)
+        self.assertEqual(test.hasCycle(listnode), True)
 
-    listnode = ListNode(1)
-    listnode.next = ListNode(2)
-    listnode.next.next = listnode
-    self.assertEqual(test.hasCycle(listnode), True)
+        listnode = ListNode(1)
+        listnode.next = ListNode(2)
+        listnode.next.next = listnode
+        self.assertEqual(test.hasCycle(listnode), True)
 
-    listnode = ListNode(1)
-    self.assertEqual(test.hasCycle(listnode), False)
+        listnode = ListNode(1)
+        self.assertEqual(test.hasCycle(listnode), False)
 
 
