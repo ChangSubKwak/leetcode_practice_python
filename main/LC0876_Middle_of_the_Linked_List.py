fae16b00154f2b6390728d773104758490777de7
cskwak <changsubkwak@nongshim.co.kr>
2024-03-07 21:51:03 +0900

    feat: (WIP) solving - 876. Middle of the Linked List

---
+++ main/LC0876_Middle_of_the_Linked_List.py
@@ -0,0 +1,8 @@
+from typing import Optional
+
+from main.ListNode import ListNode
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return None
