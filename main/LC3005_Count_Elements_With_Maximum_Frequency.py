a7faa0c82205a295aa5f4af266a1bf5caecb4eae
cskwak <changsubkwak@nongshim.co.kr>
2024-03-09 09:33:46 +0900

    feat: solved - 3005. Count Elements With Maximum Frequency

--- main/LC3005_Count_Elements_With_Maximum_Frequency.py
+++ main/LC3005_Count_Elements_With_Maximum_Frequency.py
@@ -3,4 +3,19 @@ from typing import List
 
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
-        return 5
+        count = {}
+        for num in nums:
+            if num not in count:
+                count[num] = 0
+            count[num] += 1
+
+        maxFrequency = 0
+        for key, value in count.items():
+            maxFrequency = max(maxFrequency, value)
+
+        result = 0
+        for key, value in count.items():
+            if value == maxFrequency:
+                result += value
+
+        return result
