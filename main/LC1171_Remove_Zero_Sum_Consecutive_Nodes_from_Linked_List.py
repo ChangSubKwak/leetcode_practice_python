168c688428ddf2b63aa1a2a08899e531c154960f
changsubkwak <paradise1732@gmail.com>
2024-03-12 23:48:03 +0900

    feat: (WIP) solving - 1171. Remove Zero Sum Consecutive Nodes from Linked List

---
+++ main/LC1171_Remove_Zero_Sum_Consecutive_Nodes_from_Linked_List.py
@@ -0,0 +1,8 @@
+from typing import Optional
+
+from main.ListNode import ListNode
+
+
+class Solution:
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return None
