b6682852da50a15f8b183506bf5040bca2f85b33
cskwak <changsubkwak@nongshim.co.kr>
2024-03-13 22:00:29 +0900

    test: implement test case - 2485. Find the Pivot Integer

---
+++ test/LC2485_Find_the_Pivot_Integer_Test.py
@@ -0,0 +1,11 @@
+import unittest
+
+from main.LC2485_Find_the_Pivot_Integer import Solution
+
+test = Solution()
+
+
+class SolutionTest(unittest.TestCase):
+    def test_pivotInteger(self):
+        self.assertEqual(test.pivotInteger(8), 6)
+        self.assertEqual(test.pivotInteger(1), 1)
