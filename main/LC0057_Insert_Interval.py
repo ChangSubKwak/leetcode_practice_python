e2bce280f2df74b453038369fc975b1b3d6d20ff
changsubkwak <paradise1732@gmail.com>
2024-03-18 03:28:29 +0900

    feat: solved - 57. Insert Interval

--- main/LC0057_Insert_Interval.py
+++ main/LC0057_Insert_Interval.py
@@ -3,5 +3,23 @@ from typing import List
 
 class Solution:
     @staticmethod
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        return [[1, 2], [3, 10], [12, 16]]
+    def insert(intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        length = len(intervals)
+        result = []
+        i = 0
+
+        while i < length and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
+
+        while i < length and intervals[i][0] <= newInterval[1]:
+            newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
+            i += 1
+
+        result.append(newInterval)
+
+        while i < length:
+            result.append(intervals[i])
+            i += 1
+
+        return result
