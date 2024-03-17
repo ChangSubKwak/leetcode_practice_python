6e46ae3f5fa94fdc07479254209f0012294ba3b4
changsubkwak <paradise1732@gmail.com>
2024-03-17 23:56:04 +0900

    feat: (WIP) solving - 57. Insert Interval

---
+++ main/LC0057_Insert_Interval.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        return [[1, 2], [3, 10], [12, 16]]
