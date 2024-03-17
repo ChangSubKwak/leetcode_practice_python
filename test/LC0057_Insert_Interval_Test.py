5e4660ffc27c024ef36d8fcc2f3a7fb7d122513e
changsubkwak <paradise1732@gmail.com>
2024-03-18 03:28:15 +0900

    test: fix test case - 57. Insert Interval

--- test/LC0057_Insert_Interval_Test.py
+++ test/LC0057_Insert_Interval_Test.py
@@ -1,10 +1,8 @@
 import unittest
 from main.LC0057_Insert_Interval import Solution
 
-test = Solution()
-
 
 class SolutionTest(unittest.TestCase):
     def test_insert(self):
-        self.assertEqual(test.insert([[1, 3], [6, 9]], [[2, 5]]), [[1, 5], [6, 9]])
-        self.assertEqual(test.insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]), [[1, 2], [3, 10], [12, 16]])
+        self.assertEqual(Solution.insert([[1, 3], [6, 9]], [2, 5]), [[1, 5], [6, 9]])
+        self.assertEqual(Solution.insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]), [[1, 2], [3, 10], [12, 16]])
