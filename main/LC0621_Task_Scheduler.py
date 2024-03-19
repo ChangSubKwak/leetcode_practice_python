503cf21a917544b52d8c7cc1f7bf0b1bff1b3299
changsubkwak <paradise1732@gmail.com>
2024-03-20 03:09:35 +0900

    feat: solved - 621. Task Scheduler

--- main/LC0621_Task_Scheduler.py
+++ main/LC0621_Task_Scheduler.py
@@ -4,4 +4,21 @@ from typing import List
 class Solution:
     @staticmethod
     def leastInterval(tasks: List[str], n: int) -> int:
-        return 6
+        count_dict = {}
+
+        for character in tasks:
+            if character not in count_dict:
+                count_dict[character] = 0
+            count_dict[character] += 1
+
+        sorted_dict_order_by_value_desc = sorted(count_dict.items(), key=lambda item: item[1], reverse=True)
+        _, largestValue = sorted_dict_order_by_value_desc[0]
+
+        countLargestValue = 0
+        countSum = 0
+        for _, value in sorted_dict_order_by_value_desc:
+            if value == largestValue:
+                countLargestValue += 1
+            countSum += value
+
+        return max((largestValue - 1) * (n + 1) + countLargestValue, countSum)
