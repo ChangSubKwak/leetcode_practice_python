309087404f9e28403f88065e017d8d2631b2dc0e
changsubkwak <paradise1732@gmail.com>
2024-04-07 00:47:38 +0900

    feat: solved - 1249. Minimum Remove to Make Valid Parentheses

--- main/LC1249_Minimum_Remove_to_Make_Valid_Parentheses.py
+++ main/LC1249_Minimum_Remove_to_Make_Valid_Parentheses.py
@@ -1,3 +1,28 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        return ""
+        stack = []
+        index = []
+
+        for i, c in enumerate(s):
+            if c.isalpha():
+                continue
+
+            if c == '(':
+                stack.append(c)
+                index.append(i)
+                continue
+
+            if c == ')' and len(stack) > 0 and stack[-1] == '(':
+                stack.pop()
+                index.pop()
+                continue
+
+            index.append(i)
+
+        result = ""
+        for i, c in enumerate(s):
+            if i in index:
+                continue
+            result += c
+
+        return result
