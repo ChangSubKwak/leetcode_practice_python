270880e0a687776d0deb2341c729d913794495be
changsubkwak <paradise1732@gmail.com>
2024-04-09 01:39:20 +0900

    feat: solved - 1700. Number of Students Unable to Eat Lunch

--- main/LC1700_Number_of_Students_Unable_to_Eat_Lunch.py
+++ main/LC1700_Number_of_Students_Unable_to_Eat_Lunch.py
@@ -3,4 +3,21 @@ from typing import List
 
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        return 0
+        i = 0
+
+        while True:
+            if i >= len(students):
+                break
+
+            if students[0] != sandwiches[0]:
+                i += 1
+                students = students[1:] + students[:1]
+                continue
+
+            if students[0] == sandwiches[0]:
+                students = students[1:]
+                sandwiches = sandwiches[1:]
+                i = 0
+                continue
+
+        return len(students)
