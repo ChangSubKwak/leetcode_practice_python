f5832d753c372744ca4dec0a2a949793b7d2901d
changsubkwak <paradise1732@gmail.com>
2024-04-15 00:10:49 +0900

    feat: solved - 404. Sum of Left Leaves

--- main/LC0404_Sum_of_Left_Leaves.py
+++ main/LC0404_Sum_of_Left_Leaves.py
@@ -4,5 +4,14 @@ from main.TreeNode import TreeNode
 
 
 class Solution:
+    def recur(self, root, is_left_parent):
+        if not root:
+            return 0
+
+        if not root.left and not root.right and is_left_parent:
+            return root.val
+
+        return self.recur(root.left, True) + self.recur(root.right, False)
+
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        return 24
+        return self.recur(root.left, True) + self.recur(root.right, False)
