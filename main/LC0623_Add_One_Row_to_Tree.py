76a3621a5b10d79f926b6de660228d7e43122ff3
changsubkwak <paradise1732@gmail.com>
2024-04-16 23:44:06 +0900

    feat: (WIP) solving - 623. Add One Row to Tree

---
+++ main/LC0623_Add_One_Row_to_Tree.py
@@ -0,0 +1,8 @@
+from typing import Optional
+
+from main.TreeNode import TreeNode
+
+
+class Solution:
+    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        return None
