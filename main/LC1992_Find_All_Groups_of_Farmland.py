9db780ace971c4bbcee368c3e209e1dc6e6db684
changsubkwak <paradise1732@gmail.com>
2024-04-21 03:34:45 +0900

    feat: solved - 1992. Find All Groups of Farmlang

--- main/LC1992_Find_All_Groups_of_Farmland.py
+++ main/LC1992_Find_All_Groups_of_Farmland.py
@@ -2,5 +2,43 @@ from typing import List
 
 
 class Solution:
+    def recur(self, land, y, x, visited, result):
+        if y < 0 or y >= self.row or x < 0 or x >= self.col:
+            return
+
+        if visited[y][x]:
+            return
+
+        if land[y][x] == 0:
+            return
+
+        group_col = 0
+        while x + group_col < self.col and land[y][x + group_col] == 1:
+            group_col += 1
+
+        group_row = 0
+        while y + group_row < self.row and land[y + group_row][x] == 1:
+            group_row += 1
+
+        for i in range(group_row):
+            for j in range(group_col):
+                visited[y + i][x + j] = True
+
+        result.append([y, x, y + group_row - 1, x + group_col - 1])
+
     def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
-        return []
+        self.row = len(land)
+        self.col = len(land[0])
+
+        visited = []
+        for i in range(self.row):
+            visited.append([])
+            for j in range(self.col):
+                visited[i].append(False)
+
+        result = []
+        for i in range(self.row):
+            for j in range(self.col):
+                self.recur(land, i, j, visited, result)
+
+        return result
