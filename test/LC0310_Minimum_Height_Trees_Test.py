642bc6807579c4e11b8fddff1327884c52f18c70
changsubkwak <paradise1732@gmail.com>
2024-04-24 22:29:30 +0900

    test: modify test case - 310. Minimum Height Trees

--- test/LC0310_Minimum_Height_Trees_Test.py
+++ test/LC0310_Minimum_Height_Trees_Test.py
@@ -7,8 +7,5 @@ test = Solution()
 
 class SolutionTest(unittest.TestCase):
     def test_lengthOfLIS(self):
-        self.assertEqual(test.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]), 4)
-        self.assertEqual(test.lengthOfLIS([0, 1, 0, 3, 2, 3]), 4)
-        self.assertEqual(test.lengthOfLIS([7, 7, 7, 7, 7, 7, 7]), 1)
-
-
+        self.assertEqual(test.findMinHeightTrees(4, [[1, 0], [1, 2], [1, 3]]), [1])
+        self.assertEqual(test.findMinHeightTrees(6, [[3, 0], [3, 1], [3, 2], [3, 4], [5, 4]]), [3, 4])
