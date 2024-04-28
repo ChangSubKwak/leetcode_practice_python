719f25d30cf3518d50f51b809ebe76f46b5fefb8
changsubkwak <paradise1732@gmail.com>
2024-04-29 01:30:59 +0900

    feat: solved - 834. Sum of Distances

--- main/LC0834_Sum_of_Distances_in_Tree.py
+++ main/LC0834_Sum_of_Distances_in_Tree.py
@@ -1,6 +1,31 @@
+import collections
 from typing import List
 
 
 class Solution:
     def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
-        return None
+        tree = collections.defaultdict(set)
+        result = [0] * n
+        count = [1] * n
+
+        for start, end in edges:
+            tree[start].add(end)
+            tree[end].add(start)
+
+        def dfs(root, previous):
+            for i in tree[root]:
+                if i != previous:
+                    dfs(i, root)
+                    count[root] += count[i]
+                    result[root] += result[i] + count[i]
+
+        def dfs_re_root(root, previous):
+            for i in tree[root]:
+                if i != previous:
+                    result[i] = result[root] - count[i] + n - count[i]
+                    dfs_re_root(i, root)
+
+        dfs(0, -1)
+        dfs_re_root(0, -1)
+
+        return result
