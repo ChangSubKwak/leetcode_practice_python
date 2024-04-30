b5e6d5e7a89301a1f5e963f23ab1cfce72af04eb
changsubkwak <paradise1732@gmail.com>
2024-04-30 23:29:58 +0900

    feat: solved - 2997. Minimum Number of Operations to Make Array XOR Equal to K

--- main/LC2997_Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K.py
+++ main/LC2997_Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K.py
@@ -3,4 +3,23 @@ from typing import List
 
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        return 2
+        final_value = nums[0]
+        for i in range(1, len(nums)):
+            final_value ^= nums[i]
+        final_binary_value = bin(final_value)[2:]
+
+        binary_k = bin(k)[2:]
+        width = max(len(final_binary_value), len(binary_k))
+
+        final_binary_value = f'{final_value:0{width}b}'
+        binary_k = f'{k:0{width}b}'
+
+        # print("final_binary_value", final_binary_value)
+        # print("binary_k", binary_k)
+
+        count = 0
+        for i in range(width):
+            if final_binary_value[i] != binary_k[i]:
+                count += 1
+
+        return count
