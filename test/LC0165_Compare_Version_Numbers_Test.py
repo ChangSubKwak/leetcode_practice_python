fe59609ed816285b7f55f12e6c93f1401eaa5c52
changsubkwak <paradise1732@gmail.com>
2024-05-03 23:56:03 +0900

    test: fix wrong code - 165. Compare Version Numbers

--- test/LC0165_Compare_Version_Numbers_Test.py
+++ test/LC0165_Compare_Version_Numbers_Test.py
@@ -7,10 +7,10 @@ test = Solution()
 
 class SolutionTest(unittest.TestCase):
     def test_evalRPN(self):
-        self.assertEqual(test.compareVersion(["1.01", "1.001"]), 0)
-        self.assertEqual(test.compareVersion(["1.0", "1.0.0"]), 0)
-        self.assertEqual(test.compareVersion(["0.1", "1.1"]), -1)
-        self.assertEqual(test.compareVersion(["1.2", "1.10"]), -1)
-        self.assertEqual(test.compareVersion(["3.0.4.10", "3.0.4.2"]), 1)
+        self.assertEqual(test.compareVersion("1.01", "1.001"), 0)
+        self.assertEqual(test.compareVersion("1.0", "1.0.0"), 0)
+        self.assertEqual(test.compareVersion("0.1", "1.1"), -1)
+        self.assertEqual(test.compareVersion("1.2", "1.10"), -1)
+        self.assertEqual(test.compareVersion("3.0.4.10", "3.0.4.2"), 1)
 
 
