1ba66eb267ffa8d0530b74c9116ab92476cee45d
changsubkwak <paradise1732@gmail.com>
2024-05-06 23:17:01 +0900

    feat: (WIP) solving - 2487. Remove nodes From Linked List

---
+++ main/LC2487_Remove_Nodes_From_Linked_List.py
@@ -0,0 +1,8 @@
+from typing import Optional
+
+from main.ListNode import ListNode
+
+
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return None
