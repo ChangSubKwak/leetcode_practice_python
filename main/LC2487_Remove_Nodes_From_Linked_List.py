5886e4caed3ae6255932f286a532ffff556f7306
changsubkwak <paradise1732@gmail.com>
2024-05-07 00:08:55 +0900

    feat: solved - 2487. Remove Nodes From Linked List

--- main/LC2487_Remove_Nodes_From_Linked_List.py
+++ main/LC2487_Remove_Nodes_From_Linked_List.py
@@ -5,4 +5,25 @@ from main.ListNode import ListNode
 
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        return None
+        nodes = []
+        current = head
+
+        while current != None:
+            nodes.append(current)
+            current = current.next
+
+        i = 0
+        while i + 1 < len(nodes):
+            if nodes[i].val < nodes[i + 1].val:
+                del nodes[i]
+                i = max(0, i - 1)
+                continue
+            i += 1
+
+        result = nodes[0]
+        for i, node in enumerate(nodes):
+            if i == 0:
+                continue
+            nodes[i - 1].next = nodes[i]
+
+        return result
