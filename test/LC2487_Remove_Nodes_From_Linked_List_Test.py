422a19e1f6f401cf3c86d76dc542173edef57aaa
changsubkwak <paradise1732@gmail.com>
2024-05-07 00:12:14 +0900

    test: fix test case - 2487. Remove Nodes From Linked List

--- test/LC2487_Remove_Nodes_From_Linked_List_Test.py
+++ test/LC2487_Remove_Nodes_From_Linked_List_Test.py
@@ -8,11 +8,11 @@ test = Solution()
 
 class SolutionTest(unittest.TestCase):
     def test_removeNodes1(self):
-        head = ListNode.from_list(5, 2, 13, 3, 8)
-        expected = ListNode.from_list(13, 8)
+        head = ListNode.from_list([5, 2, 13, 3, 8])
+        expected = ListNode.from_list([13, 8])
         self.assertEqual(test.removeNodes(head), expected)
 
     def test_removeNodes2(self):
-        head = ListNode.from_list(1, 1, 1, 1)
-        expected = ListNode.from_list(1, 1, 1, 1)
+        head = ListNode.from_list([1, 1, 1, 1])
+        expected = ListNode.from_list([1, 1, 1, 1])
         self.assertEqual(test.removeNodes(head), expected)
