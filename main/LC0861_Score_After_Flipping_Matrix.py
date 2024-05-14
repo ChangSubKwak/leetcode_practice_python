25a1ccccf7a3699f726cc5b6f9918e997d81e059
cskwak <changsubkwak@nongshim.co.kr>
2024-05-15 00:03:18 +0900

    feat: solved - 861. Score After Flipping Matrix

--- main/LC0861_Score_After_Flipping_Matrix.py
+++ main/LC0861_Score_After_Flipping_Matrix.py
@@ -2,4 +2,28 @@ from typing import List
 
 class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
-        return 39
+        row = len(grid)
+        col = len(grid[0])
+
+        for i in range(row):
+            if grid[i][0] == 0:
+                for j in range(col):
+                    grid[i][j] = 1 if grid[i][j] == 0 else 0
+
+        for j in range(1, col):
+            zero_of_count = 0
+            for i in range(row):
+                if grid[i][j] == 0:
+                    zero_of_count += 1
+
+            if zero_of_count > row - zero_of_count:
+                for i in range(row):
+                    grid[i][j] = 1 if grid[i][j] == 0 else 0
+
+        result = 0
+        for i in range(row):
+            binary_string = "0b" + "".join(map(str, grid[i]))
+            integer_value = int(binary_string, 2)
+            result += integer_value
+
+        return result
