6c076d12b2262ebfebd6a9b873fb9f6c858a5a96
cskwak <changsubkwak@nongshim.co.kr>
2024-05-17 01:38:17 +0900

    feat: solved - 2331. Evaluate Boolean Binary Tree

--- main/LC2331_Evaluate_Boolean_Binary_Tree.py
+++ main/LC2331_Evaluate_Boolean_Binary_Tree.py
@@ -4,4 +4,13 @@ from main.TreeNode import TreeNode
 
 class Solution:
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
-        return True
+        if root.val == 0:
+            return False
+
+        if root.val == 1:
+            return True
+
+        if root.val == 2:
+            return self.evaluateTree(root.left) or self.evaluateTree(root.right)
+
+        return self.evaluateTree(root.left) and self.evaluateTree(root.right)
