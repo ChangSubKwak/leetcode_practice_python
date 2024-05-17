711c8b8614af7bae8196c8a8e51d7072e4d5a9fc
changsubkwak <paradise1732@gmail.com>
2024-05-17 22:40:16 +0900

    feat: (WIP) solving - 1325. Delete Leaves With a Given Value

---
+++ main/LC1325_Delete_Leaves_With_a_Given_Value.py
@@ -0,0 +1,7 @@
+from typing import Optional
+from main.TreeNode import TreeNode
+
+
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        return None
