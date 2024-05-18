d1bd8303f1c9acb19dd54420ebe204370f61b2ba
changsubkwak <paradise1732@gmail.com>
2024-05-18 23:06:43 +0900

    feat: solved - 1325. Delete Leaves With a Given Value

--- main/LC1325_Delete_Leaves_With_a_Given_Value.py
+++ main/LC1325_Delete_Leaves_With_a_Given_Value.py
@@ -3,5 +3,24 @@ from main.TreeNode import TreeNode
 
 
 class Solution:
+    def recur(self, root, target, parent, is_left):
+        if not root:
+            return
+
+        self.recur(root.left, target, root, True)
+        self.recur(root.right, target, root, False)
+
+        if not root.left and not root.right and root.val == target:
+            if is_left:
+                parent.left = None
+            else:
+                parent.right = None
+
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
-        return None
+        self.recur(root.left, target, root, True)
+        self.recur(root.right, target, root, False)
+
+        if not root.left and not root.right and root.val == target:
+            root = None
+
+        return root
