7c85c1db1a459dd1476403bc94816bf7f4d858a9
cskwak <changsubkwak@nongshim.co.kr>
2024-05-20 21:46:50 +0900

    feat: solved - 1863. Sum of All Subset XOR Totals

--- main/LC1863_Sum_of_All_Subset_XOR_Totals.py
+++ main/LC1863_Sum_of_All_Subset_XOR_Totals.py
@@ -2,5 +2,23 @@ from typing import List
 
 
 class Solution:
+    def recur(self, nums, current, pos, depth, select_count, xor_value):
+        if depth > select_count:
+            return
+
+        for i in range(pos, len(nums)):
+            current.append(nums[i])
+            xor_value ^= nums[i]
+            self.sum += xor_value
+            self.recur(nums, current, i + 1, depth + 1, select_count, xor_value)
+            del current[-1]
+            xor_value ^= nums[i]
+
     def subsetXORSum(self, nums: List[int]) -> int:
-        return 28
+        # for i in range(1, len(nums) + 1):
+        self.sum = 0
+        self.recur(nums, [], 0, 0, len(nums), 0)
+
+        # print("self.sum", self.sum)
+
+        return self.sum
