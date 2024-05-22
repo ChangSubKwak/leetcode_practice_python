6b9b444e0b139d8b3560ee8b3834d6a4b5dc2710
changsubkwak <paradise1732@gmail.com>
2024-05-22 23:23:38 +0900

    feat: solved - 78. Subsets

--- main/LC0078_Subsets.py
+++ main/LC0078_Subsets.py
@@ -1,6 +1,22 @@
 from typing import List
-
+import copy
 
 class Solution:
+    result = []
+
+    def recur(self, nums, start, depth, select_count, current):
+        if depth > select_count:
+            return
+
+        for i in range(start, len(nums)):
+            current.append(nums[i])
+            # print("current", current)
+            self.result.append(copy.deepcopy(current))
+            self.recur(nums, i + 1, depth + 1, select_count, current)
+            del current[-1]
+
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        return [[]]
+        self.result = [[]]
+        self.recur(nums, 0, 0, len(nums) - 1, [])
+
+        return self.result
