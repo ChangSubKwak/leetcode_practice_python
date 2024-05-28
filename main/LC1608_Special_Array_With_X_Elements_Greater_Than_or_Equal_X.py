101261ad6b9939d502792937170947cfbb44a738
changsubkwak <paradise1732@gmail.com>
2024-05-28 21:07:08 +0900

    feat: solved - 1608. Special Array With X Elements Greater Than or Equal X

--- main/LC1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X.py
+++ main/LC1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X.py
@@ -3,4 +3,20 @@ from typing import List
 
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
+        dict_nums = {}
+
+        for num in nums:
+            if num not in dict_nums:
+                dict_nums[num] = 0
+
+            dict_nums[num] += 1
+
+        sum_nums = 0
+        for i in range(1000, -1, -1):
+            if i in dict_nums:
+                sum_nums += dict_nums[i]
+
+            if i == sum_nums:
+                return i
+
         return -1
