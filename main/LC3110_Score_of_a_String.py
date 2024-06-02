93508f407339b81cb55bbb2c1f3789360b9b7892
changsubkwak <paradise1732@gmail.com>
2024-06-02 18:37:57 +0900

    feat: solved - 3110. Score of a String

--- main/LC3110_Score_of_a_String.py
+++ main/LC3110_Score_of_a_String.py
@@ -1,3 +1,9 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        return 0
+        length = len(s)
+
+        result = 0
+        for i in range(length - 1):
+            result += abs(ord(s[i]) - ord(s[i + 1]))
+
+        return result
