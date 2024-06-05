7602a1307749336f8a8ef6d4feecc3b92c3a3943
changsubkwak <paradise1732@gmail.com>
2024-06-05 23:24:51 +0900

    feat: solved - 409. Longest Palindrome

--- main/LC0409_Longest_Palindrome.py
+++ main/LC0409_Longest_Palindrome.py
@@ -8,15 +8,20 @@ class Solution:
 
             count_map[c] += 1
 
-        # print(count_map)
+        print(count_map)
 
         even_count = 0
-        max_odd_value = 0
+        odd_count = 0
+        isExistOdd = False
 
-        for _, value in count_map.items():
+        for value in count_map.values():
             if value % 2 == 0:
                 even_count += value
             else:
-                max_odd_value = max(max_odd_value, value)
+                odd_count += (value - 1)
+                isExistOdd = True
 
-        return even_count + max_odd_value
+        if isExistOdd:
+            return even_count + odd_count + 1
+
+        return even_count + odd_count
