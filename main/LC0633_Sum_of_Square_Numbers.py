d64664bf76cf132785a9639bc2cb6c8a2517d567
changsubkwak <paradise1732@gmail.com>
2024-06-18 00:14:46 +0900

    feat: solved - 633. Sum of Square Numbers

--- main/LC0633_Sum_of_Square_Numbers.py
+++ main/LC0633_Sum_of_Square_Numbers.py
@@ -1,3 +1,18 @@
+from math import sqrt
+
+
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        return True
+        final_number = int(sqrt(pow(2, 31) - 1))
+        final_number = min(final_number, c)
+
+        square_map = set()
+
+        for i in range(final_number + 1):
+            square_map.add(i ** 2)
+
+        for i in range(final_number + 1):
+            if (c - i ** 2) in square_map:
+                return True
+
+        return False
