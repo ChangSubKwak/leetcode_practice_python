7bf4a70b665fb3bef0580c7e65907c27898405fa
changsubkwak <paradise1732@gmail.com>
2024-06-28 02:52:07 +0900

    test: modify test case - 1791. Find Center of Star Graph

--- test/LC1791_Find_Center_of_Star_Graph_Test.py
+++ test/LC1791_Find_Center_of_Star_Graph_Test.py
@@ -8,4 +8,3 @@ class TestClass(unittest.TestCase):
     def test_findCenter(self):
         self.assertEqual(test.findCenter([[1, 2], [2, 3], [4, 2]]), 2)
         self.assertEqual(test.findCenter([[1, 2], [5, 1], [1, 3], [1, 4]]), 1)
-        
