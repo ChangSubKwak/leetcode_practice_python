7962874964af1b78f4b3a3feb364b0fde7b69972
changsubkwak <paradise1732@gmail.com>
2024-06-28 09:10:12 +0900

    feat: solved - 1791. Find Center of Star Graph

--- main/LC1791_Find_Center_of_Star_Graph.py
+++ main/LC1791_Find_Center_of_Star_Graph.py
@@ -3,4 +3,18 @@ from typing import List
 
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        return 2
+        count_map = {}
+
+        for [u, v] in edges:
+            if u not in count_map:
+                count_map[u] = 0
+
+            if v not in count_map:
+                count_map[v] = 0
+
+            count_map[u] += 1
+            count_map[v] += 1
+
+        count_map = sorted(count_map.items(), key=lambda item: item[1], reverse=True)
+
+        return count_map[0][0]
