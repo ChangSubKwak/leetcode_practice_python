99c96baeaefa63ccd9f11e7ab970598c1c49f903
changsubkwak <paradise1732@gmail.com>
2024-07-02 21:51:54 +0900

    test: implement test case - 350. Intersection of Two Arrays II

---
+++ test/LC0350_Intersection_of_Two_Arrays_II_Test.py
@@ -0,0 +1,10 @@
+import unittest
+from main.LC0350_Intersection_of_Two_Arrays_II import Solution
+
+test = Solution()
+
+
+class SolutionTest(unittest.TestCase):
+    def test_intersect(self):
+        self.assertEqual(test.intersect([1, 2, 2, 1], [2, 2]), [2, 2])
+        self.assertEqual(test.intersect([4, 9, 5], [9, 4,9, 8, 4]), [4, 9])
