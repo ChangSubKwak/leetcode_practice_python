a78bdbf219523436980a748fa5733a1fb06cddcf
changsubkwak <paradise1732@gmail.com>
2024-07-03 08:41:18 +0900

    test: insert space for readability - 350. Intersection of Two Arrays II

--- test/LC0350_Intersection_of_Two_Arrays_II_Test.py
+++ test/LC0350_Intersection_of_Two_Arrays_II_Test.py
@@ -7,4 +7,4 @@ test = Solution()
 class SolutionTest(unittest.TestCase):
     def test_intersect(self):
         self.assertEqual(test.intersect([1, 2, 2, 1], [2, 2]), [2, 2])
-        self.assertEqual(test.intersect([4, 9, 5], [9, 4,9, 8, 4]), [4, 9])
+        self.assertEqual(test.intersect([4, 9, 5], [9, 4, 9, 8, 4]), [4, 9])
