e9479868ed07e87bc1e4386a2bb9d3fa523e39d6
changsubkwak <paradise1732@gmail.com>
2024-07-03 08:41:30 +0900

    feat: solved - 350. Intersection of Two Arrays II

--- main/LC0350_Intersection_of_Two_Arrays_II.py
+++ main/LC0350_Intersection_of_Two_Arrays_II.py
@@ -3,4 +3,26 @@ from typing import List
 
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return [2, 2]
+        nums_count1 = {}
+        nums_count2 = {}
+
+        for num in nums1:
+            nums_count1[num] = nums_count1.get(num, 0) + 1
+
+        for num in nums2:
+            nums_count2[num] = nums_count2.get(num, 0) + 1
+
+        if len(nums_count1) < len(nums_count2):
+            nums_count1, nums_count2 = nums_count2, nums_count1
+
+        result = []
+        for key, value in nums_count1.items():
+            if key in nums_count2:
+                iteration = min(value, nums_count2[key])
+
+                for i in range(iteration):
+                    result.append(key)
+
+        print(result)
+
+        return result
