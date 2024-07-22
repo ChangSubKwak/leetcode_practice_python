6caef1df4153a964b9c1a2d238f3fa8c8077e2ab
changsubkwak <paradise1732@gmail.com>
2024-07-22 20:46:09 +0900

    feat: solved - 1701. Average Waiting Time

--- main/LC1701_Average_Waiting_Time.py
+++ main/LC1701_Average_Waiting_Time.py
@@ -3,4 +3,21 @@ from typing import List
 
 class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
-        return 0
+        current_time = customers[0][0] + customers[0][1]
+        average = current_time - customers[0][0]
+
+        if len(customers) <= 1:
+            return average
+
+        i = 1
+        while i < len(customers):
+            if current_time < customers[i][0]:
+                current_time = customers[i][0] + customers[i][1]
+            else:
+                current_time += customers[i][1]
+
+            average += current_time - customers[i][0]
+            i += 1
+
+        return average / len(customers)
+
