8e5f04b4e72259ceded63d16f8eb8d819ae12d9b
changsubkwak <paradise1732@gmail.com>
2024-07-24 00:03:04 +0900

    test: add test case, modify incorrect code - 1636. Sort Array by Increasing Frequency

--- test/LC1636_Sort_Array_by_Increasing_Frequency_Test.py
+++ test/LC1636_Sort_Array_by_Increasing_Frequency_Test.py
@@ -1,10 +1,12 @@
 import unittest
 
-from main.LC1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points import LC1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points
+from main.LC1636_Sort_Array_by_Increasing_Frequency import Solution
 
-test = LC1636_Sort_Array_by_Increasing_Frequency()
+test = Solution()
 
 
 class SolutionTest(unittest.TestCase):
     def test_maxWidthOfVerticalArea(self):
         self.assertEqual(test.frequencySort([1, 1, 2, 2, 2, 3]), [3, 1, 1, 2, 2, 2])
+        self.assertEqual(test.frequencySort([2, 3, 1, 3, 2]), [1, 3, 3, 2, 2])
+        self.assertEqual(test.frequencySort([-1, 1, -6, 4, 5, -6, 1, 4, 1]), [5, -1, 4, 4, -6, -6, 1, 1, 1])
