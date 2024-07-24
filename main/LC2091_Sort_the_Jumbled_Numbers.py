9aefd269d3743cf9848255450b8f63d8bbdb9e10
changsubkwak <paradise1732@gmail.com>
2024-07-25 00:36:51 +0900

    feat: solved - 2091. Sort the Jumbled Numbers

--- main/LC2091_Sort_the_Jumbled_Numbers.py
+++ main/LC2091_Sort_the_Jumbled_Numbers.py
@@ -3,4 +3,22 @@ from typing import List
 
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        return []
+        original_nums = {}
+        for i, num in enumerate(nums):
+            original_nums[num] = i
+
+        converted_nums = []
+        for num in nums:
+            str_num = str(num)
+            new_num = 0
+
+            length = len(str_num)
+            for i, digit in enumerate(list(str_num)):
+                # print(i, digit)
+                new_num += mapping[int(digit)] * 10 ** (length - i - 1)
+
+            converted_nums.append([new_num, original_nums[num], num])
+
+        sorted_nums = sorted(converted_nums, key=lambda item: (item[0], item[1]))
+
+        return [sub_array[2] for sub_array in sorted_nums]
