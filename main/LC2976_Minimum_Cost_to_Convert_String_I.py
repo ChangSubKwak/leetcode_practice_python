97fedc484ec5fa7ede5dd89a8587f0cc9cf6e31c
changsubkwak <paradise1732@gmail.com>
2024-07-28 23:54:49 +0900

    feat: (WIP) solving - 2976. Minimum Cost to Convert String I

---
+++ main/LC2976_Minimum_Cost_to_Convert_String_I.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
+        return 28
