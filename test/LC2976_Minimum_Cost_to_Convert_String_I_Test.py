55a2879125c56186c24c0f298d2c8972a0988a30
changsubkwak <paradise1732@gmail.com>
2024-07-30 20:48:29 +0900

    test: modified test case - 2976. Minimum Cost to Convert String I

--- test/LC2976_Minimum_Cost_to_Convert_String_I_Test.py
+++ test/LC2976_Minimum_Cost_to_Convert_String_I_Test.py
@@ -7,6 +7,6 @@ test = Solution()
 
 class SolutionTest(unittest.TestCase):
     def test_minimumCost(self):
-        self.assertEqual(test.divideArray("abcd", "acbe", ["a", "b", "c", "c", "e", "d"], ["b", "c", "b", "e", "b", "e"], [2, 5, 5, 1, 2, 20]), 28)
-        self.assertEqual(test.divideArray("aaaa", "bbbb", ["a", "c"], ["c", "b"], [1, 2]), 12)
-        self.assertEqual(test.divideArray("abcd", "abce", ["a"], ["e"], [10000]), -1)
+        self.assertEqual(test.minimumCost("abcd", "acbe", ["a", "b", "c", "c", "e", "d"], ["b", "c", "b", "e", "b", "e"], [2, 5, 5, 1, 2, 20]), 28)
+        self.assertEqual(test.minimumCost("aaaa", "bbbb", ["a", "c"], ["c", "b"], [1, 2]), 12)
+        self.assertEqual(test.minimumCost("abcd", "abce", ["a"], ["e"], [10000]), -1)
