1661bc978743622cd8d5dfcc44e5af3c98313523
changsubkwak <paradise1732@gmail.com>
2024-08-04 23:36:31 +0900

    feat: solved - 1508. Range Sum of Sorted Subarray Sums

--- main/LC1508_Range_Sum_of_Sorted_Subarray_Sums.py
+++ main/LC1508_Range_Sum_of_Sorted_Subarray_Sums.py
@@ -3,4 +3,19 @@ from typing import List
 
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
-        return 13
+        sum_array = []
+
+        for i in range(n):
+            partial_sum = 0
+            for j in range(i, n):
+                partial_sum += nums[j]
+                sum_array.append(partial_sum)
+
+        sorted_sum_array = sorted(sum_array)
+
+        result = 0
+        for i in range(left - 1, right):
+            result += sorted_sum_array[i]
+            result %= 1000_000_007
+
+        return result
