96664461ab2156fb97032bcd66c84e08d1fb493b
changsubkwak <paradise1732@gmail.com>
2024-08-10 13:27:23 +0900

    feat: solved - 885. Spiral Matrix III

--- main/LC0885_Spiral_Matrix_III.py
+++ main/LC0885_Spiral_Matrix_III.py
@@ -3,4 +3,42 @@ from typing import List
 
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
-        return [[]]
+        checkTwoCount = 0
+        increase = 2
+        rotatePoint = [2, 3]
+
+        while rotatePoint[-1] <= 50000:
+            rotatePoint.append(rotatePoint[-1] + increase)
+            checkTwoCount += 1
+
+            if checkTwoCount == 2:
+                checkTwoCount = 0
+                increase += 1
+
+        rotatePoint = set(rotatePoint)
+
+        result = []
+        y = rStart
+        x = cStart
+
+        count = 1
+        total = rows * cols
+
+        direction_array = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        direction_index = 0
+
+        result.append([y, x])
+
+        while len(result) < total:
+            y = y + direction_array[direction_index][0]
+            x = x + direction_array[direction_index][1]
+            count += 1
+
+            if 0 <= y < rows and 0 <= x < cols:
+                result.append([y, x])
+
+            if count in rotatePoint:
+                direction_index += 1
+                direction_index %= 4
+
+        return result
