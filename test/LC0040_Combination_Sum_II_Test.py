2124f76154bbeebc4e78b64e66115e9d5932f517
changsubkwak <paradise1732@gmail.com>
2024-08-14 11:21:45 +0900

    test: modify test case - Combination Sum II

--- test/LC0040_Combination_Sum_II_Test.py
+++ test/LC0040_Combination_Sum_II_Test.py
@@ -6,5 +6,5 @@ test = Solution()
 
 class SolutionTest(unittest.TestCase):
     def test_combinationSum2(self):
-        self.assertIn(test.combinationSum2([10, 1, 2, 7, 6, 1, 5]), [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]])
-        self.assertIn(test.combinationSum2([2, 5, 2, 1, 2]), [[1, 2, 2], [5]])
+        self.assertEqual(test.combinationSum2([10, 1, 2, 7, 6, 1, 5], 8).sort(), [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]].sort())
+        self.assertEqual(test.combinationSum2([2, 5, 2, 1, 2], 5), [[1, 2, 2].sort(), [5]].sort())
