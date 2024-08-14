8ed577be735ee87a925c6c22983463c70711e70f
changsubkwak <paradise1732@gmail.com>
2024-08-14 11:21:56 +0900

    feat: solved - 40. Combination Sum II

--- main/LC0040_Combination_Sum_II.py
+++ main/LC0040_Combination_Sum_II.py
@@ -1,6 +1,25 @@
 from typing import List
-
+import copy
 
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        return []
+        answer = []
+        candidates.sort()
+        self.recur(candidates, target, 0, [], answer)
+
+        return answer
+
+    def recur(self, candidates, target, start_index, current, answer):
+        if target < 0:
+            return
+
+        if target == 0:
+            answer.append(copy.deepcopy(current))
+
+        for i in range(start_index, len(candidates)):
+            if i > start_index and candidates[i] == candidates[i - 1]:
+                continue
+
+            current.append(candidates[i])
+            self.recur(candidates, target - candidates[i], i + 1, current, answer)
+            del current[-1]
