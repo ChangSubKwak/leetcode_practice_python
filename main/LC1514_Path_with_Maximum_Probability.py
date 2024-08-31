1d095b16f4a99622abe9eac0213d47d7454b5fa3
changsubkwak <paradise1732@gmail.com>
2024-08-31 23:01:28 +0900

    feat: (WIP) solving - 1514. Path with Maximum Probability

--- main/LC1514_Path_with_Maximum_Probability.py
+++ main/LC1514_Path_with_Maximum_Probability.py
@@ -3,4 +3,13 @@ from typing import List
 
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        max_probability = [0] * n
+        max_probability[start_node] = 1
+
+        for i in range(n - 1):
+            has_update = 0
+            for j in range(len(edges)):
+                u, v = edges[j]
+                path_probability = succProb[j]
+
         return 0.25000
