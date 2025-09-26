ef8c28d7347e5737069fb7dfc087afc00160b60d
changsubkwak <paradise1732@gmail.com>
2025-09-26 23:53:58 +0900

    test: implement test case - 611. Valid Triangle Number

--- test/LC0611_Valid_Triangle_Number_Test.py
+++ test/LC0611_Valid_Triangle_Number_Test.py
@@ -1,11 +1,11 @@
 import unittest
 
-from main.LC0611_Image_Smoother import LC0611_Image_Smoother
+from main.LC0611_Valid_Triangle_Number import LC0611_Valid_Triangle_Number
 
-test = LC0611_Image_Smoother()
+test = LC0611_Valid_Triangle_Number()
 
 
-class LC0242_Valid_Anagram_Test(unittest.TestCase):
+class LC0611_Valid_Triangle_Number_Test(unittest.TestCase):
     def test_imageSmoother(self):
         self.assertEqual(test.imageSmoother([[1, 1, 1], [1, 0, 1], [1, 1, 1]]), [[0, 0, 0], [0, 0, 0], [0, 0, 0]])
         self.assertEqual(test.imageSmoother([[100, 200, 100], [200, 50, 200], [100, 200, 100]]), [[137, 141, 137], [141, 138, 141], [137, 141, 137]])
