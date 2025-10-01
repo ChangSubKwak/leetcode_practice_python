f1691ed42b0f4d7c62e6c4869dd454876427bde5
changsubkwak <paradise1732@gmail.com>
2025-10-02 00:02:29 +0900

    feat: solved - 1518. Water Bottles

--- main/LC1518_Water_Bottles.py
+++ main/LC1518_Water_Bottles.py
@@ -1,3 +1,11 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-        return 0
+        result = numBottles
+        while numBottles >= numExchange:
+            divisor = numBottles // numExchange
+            remainder = numBottles % numExchange
+
+            result += divisor
+            numBottles = divisor + remainder
+
+        return result
