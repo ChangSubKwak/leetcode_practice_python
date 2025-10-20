748d59af9dc64279a51cb08b6b19b94ca78a5c0e
changsubkwak <paradise1732@gmail.com>
2025-10-21 02:14:16 +0900

    feat: solved - 2011. Final Value of Variable After Performing Operations

--- LC2011_Final_Value_of_Variable_After_Performing_Operations.py
+++ LC2011_Final_Value_of_Variable_After_Performing_Operations.py
@@ -3,4 +3,15 @@ from typing import List
 
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
-        return 0
+        count = 0
+        for operation in operations:
+            if "--" in operation:
+                count -= 1
+                # continue
+
+            if "++" in operation:
+                count += 1
+
+            print(operation, count)
+
+        return count
