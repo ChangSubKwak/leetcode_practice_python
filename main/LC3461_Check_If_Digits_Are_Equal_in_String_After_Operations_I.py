ecd8554da448b7af36fb213d18702540e48af925
changsub <paradise1732@gmail.com>
2025-10-24 23:45:45 +0900

    feat: solved - 3461. Check If Digits Are Equal in String After Operations I

--- main/LC3461_Check_If_Digits_Are_Equal_in_String_After_Operations_I.py
+++ main/LC3461_Check_If_Digits_Are_Equal_in_String_After_Operations_I.py
@@ -1,3 +1,12 @@
 class Solution:
     def hasSameDigits(self, s: str) -> bool:
-        return True
+        while len(s) != 2:
+            first = ""
+            for i in range(0, len(s) - 1):
+                first += str((int(s[i]) + int(s[i + 1])) % 10)
+
+            s = first
+
+        print(first)
+
+        return s[0] == s[1]
