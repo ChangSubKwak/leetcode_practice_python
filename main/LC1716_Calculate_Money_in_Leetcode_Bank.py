245f07b8857584dc21f35bae81b58266cafa1dff
changsubkwak <paradise1732@gmail.com>
2025-10-26 22:42:47 +0900

    feat: solved - 1716. Calculate Money in Leetcode Bank

--- main/LC1716_Calculate_Money_in_Leetcode_Bank.py
+++ main/LC1716_Calculate_Money_in_Leetcode_Bank.py
@@ -1,3 +1,7 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        return 0
+        total = 0
+        for i in range(n):
+            total += (i % 7) + 1 * ((i // 7) + 1)
+
+        return total
