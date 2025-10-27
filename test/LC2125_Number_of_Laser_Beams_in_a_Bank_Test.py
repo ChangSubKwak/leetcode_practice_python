4483b42b16626809cf541b9c561ff7373028f7f6
changsub <paradise1732@gmail.com>
2025-10-27 22:53:33 +0900

    test: implement test case - 2125. Number of Laser Beams in a Bank

--- test/LC2125_Number_of_Laser_Beams_in_a_Bank_Test.py
+++ test/LC2125_Number_of_Laser_Beams_in_a_Bank_Test.py
@@ -1,11 +1,11 @@
 import unittest
 
-from main.LC2125_Number_of_Laser_Beams_in_a_Bank import LC2125_Number_of_Laser_Beams_in_a_Bank
+from main.LC2125_Number_of_Laser_Beams_in_a_Bank import Solution
 
-test = LC2125_Number_of_Laser_Beams_in_a_Bank()
+test = Solution()
 
 
-class LC2125_Number_of_Laser_Beams_in_a_Bank_Test(unittest.TestCase):
+class SolutionTest(unittest.TestCase):
     def test_numberOfBeams(self):
         self.assertEqual(test.numberOfBeams(["011001", "000000", "010100", "001000"]), 8)
         self.assertEqual(test.numberOfBeams(["000", "111", "000"]), 0)
