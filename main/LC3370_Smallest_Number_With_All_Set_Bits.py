eea026dfd91c1ef2637f14613752a72a0cd90fd9
changsubkwak <paradise1732@gmail.com>
2025-10-30 01:04:55 +0900

    feat: solved - 3354. Make Array Elements Equal to Zero

--- main/LC3370_Smallest_Number_With_All_Set_Bits.py
+++ main/LC3370_Smallest_Number_With_All_Set_Bits.py
@@ -1,3 +1,16 @@
 class Solution:
     def smallestNumber(self, n: int) -> int:
-        return 0
+
+        i = 0
+        left = right = -1
+
+        while True:
+            left = pow(2, i) - 1
+            right = pow(2, i + 1) - 1
+
+            if left <= n <= right:
+                break
+
+            i += 1
+
+        return right
