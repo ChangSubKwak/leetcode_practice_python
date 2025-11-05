2b2132d035c9c17d6ac7a92b71c17abcbb12bd13
changsub <paradise1732@gmail.com>
2025-11-05 23:42:39 +0900

    test: implement test case - 3318. Find X-Sum of All K-Long Subarrays II

---
+++ test/LC3321_Find_X_Sum_of_All_K_Long_Subarrays_II_Test.py
@@ -0,0 +1,10 @@
+import unittest
+from main.LC3321_Find_X_Sum_of_All_K_Long_Subarrays_II import Solution
+
+test = Solution()
+
+
+class SolutionTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(test.findXSum([1, 1, 2, 2, 3, 4, 2, 3], 6, 2),  [6, 10, 12])
+        self.assertEqual(test.findXSum([3, 8, 7, 8, 7, 5], 2, 2),  [11, 15, 15, 15, 12])
