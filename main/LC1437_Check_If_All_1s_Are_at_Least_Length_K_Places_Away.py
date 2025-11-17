e61314e5ae23d29c1c2498945f4333a1815bc24a
changsubkwak <paradise1732@gmail.com>
2025-11-18 01:43:24 +0900

    feat: (WIP) solving - 1437. Check If All 1's Are at Least Length K Places Away

---
+++ main/LC1437_Check_If_All_1s_Are_at_Least_Length_K_Places_Away.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        diff_pos = []
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                diff_pos.append(i)
+
+        # print(f"diff_pos : {diff_pos}")
+
+        for i in range(len(diff_pos) - 1):
+            if abs(diff_pos[i + 1] - diff_pos[i] - 1) < k:
+                return False
+
+        return True
