0be6024dd24e0e3036e3bbe317faf871768e0e83
changsubkwak <paradise1732@gmail.com>
2025-12-06 00:50:06 +0900

    feat: solved - 3432. Count Partitions with Even Sum Difference

--- main/LC3432_Count_Partitions_with_Even_Sum_Difference.py
+++ main/LC3432_Count_Partitions_with_Even_Sum_Difference.py
@@ -3,4 +3,15 @@ from typing import List
 
 class Solution:
     def countPartitions(self, nums: List[int]) -> int:
-        return 0
+        nums_sum = sum(nums)
+        accumulated_sum = 0
+
+        count = 0
+        for i in range(len(nums) - 1):
+            accumulated_sum += nums[i]
+            if (nums_sum -  2*accumulated_sum) % 2 == 0:
+                count += 1
+
+            # print(accumulated_sum, (nums_sum -  accumulated_sum), count)
+
+        return count
