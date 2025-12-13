6203786e54310e33cfccb3a31bfa502631aa9e1f
changsubkwak <paradise1732@gmail.com>
2025-12-13 19:27:03 +0900

    test: implement test case - 3606. Coupon Code Validator

---
+++ test/LC3606_Coupon_Code_Validator_Test.py
@@ -0,0 +1,19 @@
+import unittest
+from main.LC3606_Coupon_Code_Validator import Solution
+
+test = Solution()
+
+
+class SolutionTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(test.validateCoupons(
+            ["SAVE20","","PHARMA5","SAVE@20"],
+            ["restaurant","grocery","pharmacy","restaurant"],
+            [True, True, True, True]
+        ), ["PHARMA5","SAVE20"])
+
+        self.assertEqual(test.validateCoupons(
+            ["GROCERY15","ELECTRONICS_50","DISCOUNT10"],
+            ["grocery","electronics","invalid"],
+            [False, True, True]
+        ), ["ELECTRONICS_50")
