de8b85d507e1925c46d69b309f756b17393a2315
changsubkwak <paradise1732@gmail.com>
2025-12-16 00:37:30 +0900

    feat: solved - 2110. Number of Smooth Descent Periods of a Stock

--- main/LC2110_Number_of_Smooth_Descent_Periods_of_a_Stock.py
+++ main/LC2110_Number_of_Smooth_Descent_Periods_of_a_Stock.py
@@ -3,4 +3,23 @@ from typing import List
 
 class Solution:
     def getDescentPeriods(self, prices: List[int]) -> int:
-        return 0
+        contiguous_list = []
+
+        i = 0
+        while i < (len(prices) - 1):
+
+            contiguous_count = 0
+            while i < (len(prices) - 1) and prices[i] - prices[i + 1] == 1:
+                contiguous_count += 1
+                i += 1
+
+            if contiguous_count > 0:
+                contiguous_list.append(contiguous_count)
+
+            i += 1
+
+        answer = len(prices)
+        for num in contiguous_list:
+            answer += int(num * (num + 1) / 2)
+
+        return answer
