e6ee1a70ef92400cf08e629e1e13a56693b7ee2f
changsubkwak <paradise1732@gmail.com>
2025-12-23 00:09:51 +0900

    test: implement test case - 960. Delete Columns to Make Sorted III

---
+++ test/LC0960_Delete_Columns_to_Make_Sorted_III_Test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from main.LC0960_Delete_Columns_to_Make_Sorted_II import Solution
+
+test = Solution()
+
+
+class SolutionTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(test.minDeletionSize(["babca", "bbazb"]),3)
+        self.assertEqual(test.minDeletionSize(["edcba"]),4)
+        self.assertEqual(test.minDeletionSize(["ghi", "def", "abc"]),0)
