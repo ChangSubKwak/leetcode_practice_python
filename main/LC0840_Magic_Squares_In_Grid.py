eaeb46bdef568a4c9e9ad6da1c8237d72309f5d6
changsubkwak <paradise1732@gmail.com>
2025-12-30 09:16:07 +0900

    feat: (WIP) solving - 840. Magic Squares In Grid (retry)

--- main/LC0840_Magic_Squares_In_Grid.py
+++ main/LC0840_Magic_Squares_In_Grid.py
@@ -3,4 +3,14 @@ from typing import List
 
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        if len(grid) < 3:
+            return 0
+
+        if len(grid[0]) < 3:
+            return 0
+
+        for i in range(3):
+            for j in range(3):
+                pass
+
         return 0
