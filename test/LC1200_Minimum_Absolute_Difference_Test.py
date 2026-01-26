108f645459da9ad647aa5e07c424d27740f0c332
changsubkwak <paradise1732@gmail.com>
2026-01-26 23:02:40 +0900

    test: implement test case - 1200. Minimum Absolute Difference

---
+++ test/LC1200_Minimum_Absolute_Difference_Test.py
@@ -0,0 +1,11 @@
+import unittest
+
+from main.LC1200_Minimum_Absolute_Difference import Solution
+
+test = Solution()
+
+
+class SolutionTest(unittest.TestCase):
+
+  def test_uniqueOccurrences(self):
+    self.assertEqual(test.minimumAbsDifference([4, 2, 1, 3]), )
