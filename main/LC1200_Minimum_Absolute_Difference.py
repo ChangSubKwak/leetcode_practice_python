359f53ddcaedd46d1fe339196e3f5cb232f97c76
changsubkwak <paradise1732@gmail.com>
2026-01-28 00:20:44 +0900

    feat: solved - 1200. Minimum Absolute Difference

--- main/LC1200_Minimum_Absolute_Difference.py
+++ main/LC1200_Minimum_Absolute_Difference.py
@@ -2,4 +2,18 @@ from typing import List
 
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-        return 0
+        arr.sort()
+
+        length = len(arr)
+        min_value = 2 * (10 ** 6)
+        for i in range(1, length):
+            min_value = min(min_value, abs(arr[i - 1] - arr[i]))
+
+        result = []
+        for i in range(1, length):
+            if min_value == abs(arr[i - 1] - arr[i]):
+                first = min(arr[i - 1], arr[i])
+                second = max(arr[i - 1], arr[i])
+                result.append([first, second])
+
+        return result
