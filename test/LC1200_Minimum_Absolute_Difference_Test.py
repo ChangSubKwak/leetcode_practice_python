3a211425f8e0f29e24a93d3b50485284503f74b0
changsubkwak <paradise1732@gmail.com>
2026-01-28 00:20:27 +0900

    test: fix test case - 1200. Minimum Absolute Difference

--- test/LC1200_Minimum_Absolute_Difference_Test.py
+++ test/LC1200_Minimum_Absolute_Difference_Test.py
@@ -6,6 +6,7 @@ test = Solution()
 
 
 class SolutionTest(unittest.TestCase):
-
-  def test_uniqueOccurrences(self):
-    self.assertEqual(test.minimumAbsDifference([4, 2, 1, 3]), )
+    def test_uniqueOccurrences(self):
+        self.assertEqual(test.minimumAbsDifference([4, 2, 1, 3]), [[1, 2], [2, 3], [3, 4]])
+        self.assertEqual(test.minimumAbsDifference([1, 3, 6, 10, 15]), [[1, 3]])
+        self.assertEqual(test.minimumAbsDifference([3, 8, -10, 23, 19, -4, -14, 27]), [[-14, -10], [19, 23], [23, 27]])
