3302c10c521ae3faafa81eca1055670a74b2ecfd
changsubkwak <paradise1732@gmail.com>
2026-02-01 01:09:31 +0900

    feat: solved - 744. Find Smallest Letter Greater Than Target

--- main/LC0744_Find_Smallest_Letter_Greater_Than_Target.py
+++ main/LC0744_Find_Smallest_Letter_Greater_Than_Target.py
@@ -3,4 +3,20 @@ from typing import List
 
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        return ""
+        letters.append(target)
+        set_of_letters = set(letters)
+        list_of_set = list(set_of_letters)
+        list_of_set.sort()
+
+        # list_of_set.append(target)
+        # list_of_set.sort()
+
+        if (list_of_set.index(target) + 1) == len(list_of_set):
+            return list_of_set[0]
+
+        index = list_of_set.index(target)
+
+        # print("index", index)
+        # print("list_of_set", list_of_set)
+
+        return list_of_set[index + 1]
