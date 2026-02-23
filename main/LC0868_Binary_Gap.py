cb212a6d08fbedfe9f05abc2d7b53c6ede4e00ca
changsubkwak <paradise1732@gmail.com>
2026-02-24 00:50:27 +0900

    feat: solved - 868. Binary Gap

--- main/LC0868_Binary_Gap.py
+++ main/LC0868_Binary_Gap.py
@@ -1,3 +1,20 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
-        return 0
+        if n <= 2:
+            return 0
+
+        binary = bin(n)[2:]
+
+        indices = []
+        for i, ch in enumerate(binary):
+            if ch == '1':
+                indices.append(i)
+
+        # print(indices)
+
+        max_value = 0
+        for i in range(len(indices) - 1):
+            max_value = max(max_value, indices[i + 1] - indices[i])
+            # print(max_value, indices[i + 1] - indices[i])
+
+        return max_value
