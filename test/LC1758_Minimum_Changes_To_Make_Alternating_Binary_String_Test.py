9b9eedbc63074a8a07c32adf9285b8c84a4e75f5
changsub <paradise1732@gmail.com>
2026-03-05 23:15:44 +0900

    test: implement test case - 1758. Minimum Changes To Make Alternating Binary String

--- test/LC1758_Minimum_Changes_To_Make_Alternating_Binary_String_Test.py
+++ test/LC1758_Minimum_Changes_To_Make_Alternating_Binary_String_Test.py
@@ -1,11 +1,11 @@
 import unittest
 
-from main.LC1758_Minimum_Changes_To_Make_Alternating_Binary_String import LC1758_Minimum_Changes_To_Make_Alternating_Binary_String
+from main.LC1758_Minimum_Changes_To_Make_Alternating_Binary_String import Solution
 
-test = LC1758_Minimum_Changes_To_Make_Alternating_Binary_String()
+test = Solution()
 
 
-class LC1758_Minimum_Changes_To_Make_Alternating_Binary_String_Test(unittest.TestCase):
+class SolutionTest(unittest.TestCase):
     def test_minOperations(self):
         self.assertEqual(test.minOperations("0100"), 1)
         self.assertEqual(test.minOperations("10"), 0)
