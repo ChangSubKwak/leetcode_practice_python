14eb2f23aa05492dcfc58602d4a2124811583233
changsubkwak <paradise1732@gmail.com>
2026-03-18 23:59:40 +0900

    feat: (WIP) solving - 3296. Minimum Number of Seconds to Make Mountain Height Zero #2

--- main/LC3296_Minimum_Number_of_Seconds_to_Make_Mountain_Height_Zero.py
+++ main/LC3296_Minimum_Number_of_Seconds_to_Make_Mountain_Height_Zero.py
@@ -3,4 +3,5 @@ from typing import List
 
 class Solution:
     def minNumberOfSeconds(self, mountainHeight: int, workerTimes: List[int]) -> int:
+        print(mountainHeight, workerTimes)
         return 0
